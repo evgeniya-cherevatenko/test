8870bd91605d5adc6c2802d1bc089d2aa1d4e23b
evgeniya-cherevatenko <evgeniya.cherevatenko@gmail.com>
2020-10-29 10:17:03 +0000

    Update test.js
    screen_transition event, external script

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-window.harness_event.on('Lead', function (params) {
-  console.log('harness_event', 'Lead', params)
-})
-
-window.harness_event.on('InitiateCheckout', function (params) {
-  console.log('harness_event', 'InitiateCheckout', params)
-})
-
-window.harness_event.on('CompleteRegistration', function (params) {
-  console.log('harness_event', 'CompleteRegistration', params)
+window.harness_event.on('screen_transition', function (params) {
+  console.log('screen_transition event, external script', params)
 })
